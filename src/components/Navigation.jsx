d57ddc55b563a8c83251376c509e2a6a06154c98
Donald Heal <donnie.heal@gmail.com>
2022-07-26 22:53:41 +1000

    changed up nav bar should give give correct options reflecting if a user is signed in or not. waiting for sign up to pass to backend to confirm

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useGlobalState } from "../utils/stateContext";
 
 const Navigation = () => {
-    return (
-        <nav class="nav">
-            <Link to='/'>Home</Link>
-            <Link to='/bookings'>Bookings</Link>
-            <Link to='/prices'>Prices</Link>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/login'>Log in</Link>
-            <Link to='/signup'>Sign up</Link>
-        </nav>
-    )
-}
+  const { store, dispatch } = useGlobalState();
+  const { loggedInUser } = store;
 
-export default Navigation
+  const handleLogout = () => {
+    sessionStorage.removeItem("username");
+    sessionStorage.removeItem("token");
+    dispatch({
+      type: "setLoggedInUser",
+      data: null,
+    });
+    dispatch({
+      type: "setToken",
+      data: null,
+    });
+  };
+
+  return (
+    <nav class="nav">
+      <Link to="/">Home</Link>
+      <Link to="/bookings">Bookings</Link>
+      <Link to="/prices">Prices</Link>
+      {!loggedInUser && <Link to="/login">Login</Link>}
+      {!loggedInUser && <Link to="/signup">Sign Up</Link>}
+      {loggedInUser && (
+        <Link to="/account" onClick={handleLogout}>
+          Account
+        </Link>
+      )}
+      {loggedInUser && (
+        <Link to="/logout" onClick={handleLogout}>
+          Logout
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Navigation;

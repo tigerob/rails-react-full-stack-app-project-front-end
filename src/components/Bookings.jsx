a1f479b0ac79e233005a5721f4d070203fcd3f40
tigerob <obrienxyz@gmail.com>
2022-07-27 21:19:10 +1000

    Amend Bookings.jsx to link to Account

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -5,7 +5,7 @@ import { useGlobalState } from "../utils/stateContext";
 const Bookings = () => {
     // const {store, dispatch} = useGlobalState()
     const {store} = useGlobalState()
-    const {loggedInUser, bookingsList} = store
+    const {loggedInUser} = store
 
     return (
         <>
@@ -13,9 +13,8 @@ const Bookings = () => {
             <p>Developer note: The current user is:</p>
             { loggedInUser && <p>{loggedInUser}</p> }
             <Link to="/bookings/new">Make new booking</Link>
-            <h3>Upcoming and past bookings</h3>
-            <p>Developer note: display user bookings</p>
-            <p>{bookingsList}</p>
+            <p>View all upcoming and past bookings in:</p>
+            <Link to="/account">Account</Link>
         </>
     )
 }

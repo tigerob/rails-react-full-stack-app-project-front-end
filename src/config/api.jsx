959e6e05a4ebbd2d5e747953ddf9c5f1144414ad
Donald Heal <donnie.heal@gmail.com>
2022-08-06 09:55:41 +1000

    working .env file also been added to git ignore with the backend url

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const miaMusicAPI = axios.create({
-  baseURL: "https://mia-music-studios-api.herokuapp.com/",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 miaMusicAPI.interceptors.request.use((req) => {
